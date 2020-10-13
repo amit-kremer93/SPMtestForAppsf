6f1f9ba6690d60340620cf2961bdef7d6a70a0ba
amit.kremer <amit.kremer@appsflyer.com>
2020-10-13 13:40:02 +0300

    6

--- Package.swift
+++ Package.swift
@@ -20,9 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AppsFlyerLib",
-            dependencies: []),
-        .testTarget(
-            name: "AppsFlyerLib.frameworkTests",
-            dependencies: ["AppsFlyerLib.framework"]),
+            dependencies: [],
+            path: "Versions/Current"),
     ]
 )
--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import AppsFlyerLib_frameworkTests
-
-var tests = [XCTestCaseEntry]()
-tests += AppsFlyerLib_frameworkTests.allTests()
-XCTMain(tests)
