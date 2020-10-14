c1505606ec88f045a404331f6e9a3a2cc7e13d52
amit.kremer <amit.kremer@appsflyer.com>
2020-10-14 13:19:16 +0300

    16

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "AppsFlyerLibSPM",
+    name: "AppsFlyerLib",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "AppsFlyerLibSPM",
+            name: "AppsFlyerLib",
             targets: ["AppsFlyerLib"]),
     ],
     dependencies: [
